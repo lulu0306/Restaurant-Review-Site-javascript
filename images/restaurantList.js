1c760b92215b03958f150cf1877140bcf395b6b4
lulu0306 <malumel03@gmail.com>
2019-07-23 06:19:55 -0500

    Initial commit

---
+++ images/restaurantList.js
@@ -0,0 +1,6 @@
+var restaurants =[
+{title:'Baran',location:{lat:0,lng:0}},
+{title:'Restaurant 2',location:{lat:0,lng:0}},
+{title:'Restaurant 3',location:{lat:0,lng:0}},
+{title:'Restaurant 4',location:{lat:0,lng:0}},
+]
